cade3ea2b20c37afdebe43b0bcbad821db3d45af
y0ngjaenious <47391032+y0ngjaenious@users.noreply.github.com>
2020-12-16 15:19:26 +0900

    Update two_sum.py

--- DataStructure/Map/two_sum.py
+++ DataStructure/Map/two_sum.py
@@ -1,4 +1,4 @@
-question link: https://leetcode.com/problems/two-sum/
+# question link: https://leetcode.com/problems/two-sum/
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dic = {}
