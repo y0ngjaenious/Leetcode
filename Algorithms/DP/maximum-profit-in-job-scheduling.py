db177a3332deab83cb4e97565e7d3e88fa588b89
y0ngjaenious <47391032+y0ngjaenious@users.noreply.github.com>
2021-02-07 22:12:16 +0900

    Create maximum-profit-in-job-scheduling.py

---
+++ Algorithms/DP/maximum-profit-in-job-scheduling.py
@@ -0,0 +1,16 @@
+# question link: 
+
+import bisect
+
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        jobs = sorted(zip(startTime, endTime, profit), key=lambda x: x[1])
+        dp = [[0, 0]]
+        
+        for s, e, p in jobs:
+            idx = bisect.bisect(dp, [s + 1]) - 1
+            cur = dp[idx][1] + p
+            if cur > dp[-1][1]:
+                dp.append([e, cur])
+        
+        return dp[-1][1]
